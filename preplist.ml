63978156bf6f940a9329d49c478390308fdc0fe5
Igor Khotin <khotin@gmx.com>
2021-04-16 00:18:07 +0300

    [+] preplist

---
+++ preplist.ml
@@ -0,0 +1,42 @@
+>> docker
+>> kubernetes
+>> helm
+>> istio
+>> kafka
+>> AWS
+ > Cloud Formation
+ > Lambdas
+>> Google App Engine
+>> Terraform
+
+>> GoF patterns
+
+>> GRASP
+
+>> SOLID
+
+>> Enterprise patterns
+
+>> Microservices patterns
+
+> DRY
+> KISS
+> YAGNI
+
+> WebFlux
+> Apache Camel
+> Spring Cloud
+> Spring Integration
+> RabbitMQ
+> JMS
+> Redis
+> Memcached
+> Ansible
+> Chef
+> Puppet
+
+> React
+> Bootstrap
+
+> GraphQL
+
